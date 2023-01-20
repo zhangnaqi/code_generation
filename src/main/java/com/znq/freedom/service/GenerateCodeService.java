63b704c7f8e02b2bb977bf69c0b21b2e3c55aaba
zhangnaqi <zhangnaqi2002@163.com>
2023-01-20 17:18:17 +0800

    对GenerateCodeService.java中重复代码块进行抽取

--- src/main/java/com/znq/freedom/service/GenerateCodeService.java
+++ src/main/java/com/znq/freedom/service/GenerateCodeService.java
@@ -5,13 +5,12 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.StringWriter;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -26,7 +25,6 @@ import com.znq.freedom.model.StaticInfo;
 import com.znq.freedom.model.TableClass;
 import com.znq.freedom.model.TemplateConfig;
 import com.znq.freedom.model.TemplatesType;
-import com.znq.freedom.utils.ChangeCharset;
 import com.znq.freedom.utils.DBUtils;
 import com.znq.freedom.utils.ObjMapUtils;
 import com.znq.freedom.utils.Result;
@@ -61,58 +59,9 @@ public class GenerateCodeService {
             DatabaseMetaData metaData = connection.getMetaData();
             // 遍历前端传过来的要生成的表list
             for (TableClass tableClass : tableClassList) {
-                // 通过表名获取表中的字段
-                ResultSet columns = metaData.getColumns(connection.getCatalog(), null, tableClass.getTableName(), null);
-                // 根据表名获取表中的主键
-                ResultSet primaryKeys = metaData.getPrimaryKeys(connection.getCatalog(), null,
-                        tableClass.getTableName());
-                // 主键
-                PrimaryKey pk = new PrimaryKey();
-                while (primaryKeys.next()) {
-                    pk.setPkName(primaryKeys.getString("COLUMN_NAME"));
-                    // 主键是否递增
-                    String sql = "select " + pk.getPkName() + " from " + tableClass.getTableName();
-                    PreparedStatement prepareStatement = connection.prepareStatement(sql);
-                    ResultSet res = prepareStatement.executeQuery();
-                    ResultSetMetaData metaData1 = res.getMetaData();
-                    if (metaData1.isAutoIncrement(1)) {
-                        // System.out.println("自动递增id");
-                        pk.setPKAuto(true);
-                    }
-                    // 主键在bean类中的存储
-                    String newColumnName = pk.getPkName().replace(tableClass.getColumnsPrefix(), "")
-                            .replace(tableClass.getColumnsSuffix(), "");
-                    pk.setPkPropertyName(CaseFormat.LOWER_UNDERSCORE
-                            .to(CaseFormat.UPPER_CAMEL, newColumnName));
-                }
-                primaryKeys.first();
-                tableClass.setPk(pk);
-                List<ColumnClass> columnClassList = new ArrayList<>();
-                // 循环表中的字段
-                while (columns.next()) {
-                    // 字段名
-                    String column_name = columns.getString("COLUMN_NAME");
-                    // 字段类型
-                    String type_name = columns.getString("TYPE_NAME");
-                    // 备注
-                    String remarks = columns.getString("REMARKS");
-                    // 主键类型添加
-                    if (pk.getPkName().equals(column_name)) {
-                        pk.setPkType(type_name);
-                    }
-                    // 将获取到的数据存储起来
-                    ColumnClass columnClass = new ColumnClass();
-                    columnClass.setRemark(remarks);
-                    columnClass.setColumnName(column_name);
-                    columnClass.setType(type_name);
-                    // 处理前缀和后缀
-                    String newColumnName = column_name.replace(tableClass.getColumnsPrefix(), "")
-                            .replace(tableClass.getColumnsSuffix(), "");
-                    columnClass.setPropertyName(CaseFormat.LOWER_UNDERSCORE
-                            .to(CaseFormat.UPPER_CAMEL, newColumnName));
-                    columnClassList.add(columnClass);
-                }
-                tableClass.setColumns(columnClassList);
+                // 对数据库信息的处理
+                this.getDBInfo(connection, metaData, tableClass);
+                // 获取包名和存储路径
                 String packageName = StaticInfo.GLOBAL_CONFIG.getPackageName();
                 String path = new StringBuffer()
                         .append(realPath)
@@ -150,59 +99,9 @@ public class GenerateCodeService {
             Connection connection = DBUtils.getConnection();
             // 获取数据库详细信息
             DatabaseMetaData metaData = connection.getMetaData();
-            // 通过表名获取表中的字段
-            ResultSet columns = metaData.getColumns(connection.getCatalog(),
-                    null, tableClass.getTableName(), null);
-            // 根据表名获取表中的主键
-            ResultSet primaryKeys = metaData.getPrimaryKeys(connection.getCatalog(),
-                    null, tableClass.getTableName());
-            // 主键
-            PrimaryKey pk = new PrimaryKey();
-            while (primaryKeys.next()) {
-                pk.setPkName(primaryKeys.getString("COLUMN_NAME"));
-                // 主键是否递增
-                String sql = "select " + pk.getPkName() + " from " + tableClass.getTableName();
-                PreparedStatement prepareStatement = connection.prepareStatement(sql);
-                ResultSet res = prepareStatement.executeQuery();
-                ResultSetMetaData metaData1 = res.getMetaData();
-                if (metaData1.isAutoIncrement(1)) {
-                    // System.out.println("自动递增id");
-                    pk.setPKAuto(true);
-                }
-                // 主键在bean类中的存储
-                String newColumnName = pk.getPkName().replace(tableClass.getColumnsPrefix(), "")
-                        .replace(tableClass.getColumnsSuffix(), "");
-                pk.setPkPropertyName(CaseFormat.LOWER_UNDERSCORE
-                        .to(CaseFormat.UPPER_CAMEL, newColumnName));
-            }
-            primaryKeys.first();
-            tableClass.setPk(pk);
-            List<ColumnClass> columnClassList = new ArrayList<>();
-            // 循环表中的字段
-            while (columns.next()) {
-                // 字段名
-                String column_name = columns.getString("COLUMN_NAME");
-                // 字段类型
-                String type_name = columns.getString("TYPE_NAME");
-                // 备注
-                String remarks = columns.getString("REMARKS");
-                // 主键类型添加
-                if (pk.getPkName().equals(column_name)) {
-                    pk.setPkType(type_name);
-                }
-                // 将获取到的数据存储起来
-                ColumnClass columnClass = new ColumnClass();
-                columnClass.setRemark(remarks);
-                columnClass.setColumnName(column_name);
-                columnClass.setType(type_name);
-                // 处理前缀和后缀
-                String newColumnName = column_name.replace(tableClass.getColumnsPrefix(), "")
-                        .replace(tableClass.getColumnsSuffix(), "");
-                columnClass.setPropertyName(CaseFormat.LOWER_UNDERSCORE
-                        .to(CaseFormat.UPPER_CAMEL, newColumnName));
-                columnClassList.add(columnClass);
-                tableClass.setColumns(columnClassList);
-            }
+            // 对数据库信息的处理
+            this.getDBInfo(connection, metaData, tableClass);
+            // 获取包名
             String packageName = StaticInfo.GLOBAL_CONFIG.getPackageName();
             // 数据
             Map<String, Object> convertObjToMap = ObjMapUtils.convertObjToMap(tableClass);
@@ -252,6 +151,76 @@ public class GenerateCodeService {
         StringWriter stringWriter = new StringWriter();
         template.process(map, stringWriter);
         return stringWriter.toString();
+    }
+
+    /**
+     * 获取数据库详细信息
+     * 
+     * @param connection 数据库连接
+     * @param metaData   数据库详细信息
+     * @param tableClass 存储数据库的表名等信息
+     */
+    private void getDBInfo(Connection connection,
+            DatabaseMetaData metaData,
+            TableClass tableClass) {
+        try {
+            // 通过表名获取表中的字段
+            ResultSet columns = metaData.getColumns(connection.getCatalog(), null, tableClass.getTableName(), null);
+            // 根据表名获取表中的主键
+            ResultSet primaryKeys = metaData.getPrimaryKeys(connection.getCatalog(), null,
+                    tableClass.getTableName());
+            // 主键
+            PrimaryKey pk = new PrimaryKey();
+            while (primaryKeys.next()) {
+                pk.setPkName(primaryKeys.getString("COLUMN_NAME"));
+                // 主键是否递增
+                String sql = "select " + pk.getPkName() + " from " + tableClass.getTableName();
+                PreparedStatement prepareStatement = connection.prepareStatement(sql);
+                ResultSet res = prepareStatement.executeQuery();
+                ResultSetMetaData metaData1 = res.getMetaData();
+                if (metaData1.isAutoIncrement(1)) {
+                    // System.out.println("自动递增id");
+                    pk.setPKAuto(true);
+                }
+                // 主键在bean类中的存储
+                String newColumnName = pk.getPkName().replace(tableClass.getColumnsPrefix(), "")
+                        .replace(tableClass.getColumnsSuffix(), "");
+                pk.setPkPropertyName(CaseFormat.LOWER_UNDERSCORE
+                        .to(CaseFormat.UPPER_CAMEL, newColumnName));
+            }
+            primaryKeys.first();
+            tableClass.setPk(pk);
+            List<ColumnClass> columnClassList = new ArrayList<>();
+            // 循环表中的字段
+            while (columns.next()) {
+                // 字段名
+                String column_name = columns.getString("COLUMN_NAME");
+                // 字段类型
+                String type_name = columns.getString("TYPE_NAME");
+                // 备注
+                String remarks = columns.getString("REMARKS");
+                // 主键类型添加
+                if (pk.getPkName().equals(column_name)) {
+                    pk.setPkType(type_name);
+                }
+                // 将获取到的数据存储起来
+                ColumnClass columnClass = new ColumnClass();
+                columnClass.setRemark(remarks);
+                columnClass.setColumnName(column_name);
+                columnClass.setType(type_name);
+                // 处理前缀和后缀
+                String newColumnName = column_name.replace(tableClass.getColumnsPrefix(), "")
+                        .replace(tableClass.getColumnsSuffix(), "");
+                columnClass.setPropertyName(CaseFormat.LOWER_UNDERSCORE
+                        .to(CaseFormat.UPPER_CAMEL, newColumnName));
+                columnClassList.add(columnClass);
+            }
+            tableClass.setColumns(columnClassList);
+        } catch (SQLException e) {
+            log.info("获取数据库表详细信息失败 ====》", tableClass.getTableName());
+            e.printStackTrace();
+            throw new RuntimeException("获取数据库详细信息失败，表名： ====》 " + tableClass.getTableName());
+        }
     }
 
 }
